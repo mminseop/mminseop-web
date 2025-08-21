77b522b9c21c576b70a1abf17fc51a007485fb13
mminseop <alstjq903@naver.com>
2025-08-21 13:20:48 +0900

    ✨ Feat: 기능추가1

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>mminseop-wep</h1>
+      <p>기능 추가1</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
